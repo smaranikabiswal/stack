3ff47c6bcebf6beac5a9e0a523e4ac8a9b9bc6be
smaranika biswal <23btcse80@suiit.ac.in>
2025-06-02 22:01:02 +0530

    add codes

---
+++ dailytemperature.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class dailytemperature {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] temps = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] result = dailyTemperatures(temps);
+        for (int r : result) System.out.print(r + " "); // 1 1 4 2 1 1 0 0
+    }
+}
